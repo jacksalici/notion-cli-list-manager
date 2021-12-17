f4ff0a91e24ded41e2c957ee4d9553cd14a78c03
Giacomo Salici <giambo2000@gmail.com>
2021-12-17 19:59:46 +0100

    file_helper

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,24 @@ import requests
 import toml
 import json
 
+class file_helper():
+    token_file = "src/token.json"
+    config_file = "src/token.json"
+    pages_file = "src/pages.json"
 
+    def get(file):
+        with open(file, "r") as file_text:
+            return file_text
+    
+    def set(file, text):
+        with open(file, "a") as file_text:
+            file_text.write(text)
 
 class api_helper():
     api_url_pre = "https://api.notion.com/v1/"
     
-    with open ("src/token.json", "r") as token_file:
-        NOTION_API_KEY = json.load(token_file).get("token")
+ 
+    NOTION_API_KEY = json.load(file_helper.get(file_helper.stoken_file)).get("token")
 
     with open ("src/config.toml", "r") as config_file:
         DATABASE_ID = toml.load(config_file).get("databases").get("id")
@@ -18,26 +29,17 @@ class api_helper():
         "Notion-Version": "2021-08-16",
         "Content-Type": "application/json"
     }
-
-    def __init__(self) -> None:
-        pass
     
     def new_page(self, title):
 
         data = '{"parent":{"database_id":"'+self.DATABASE_ID + \
         '"},"properties":{"Name":{"title":[{"text":{"content":"'+title+'"}}]}}}'
-        r = self.api_post(self, "pages", data)
+        return self.api_post(self, "pages", data)
 
 
     def get_pages(self):
-       
-        r = self.api_post(self, "databases/" + self.DATABASE_ID + "/query", "")
-        i = 0
-        for page in r["results"]:
-            path = page.get("properties").get("Name").get("title")
-            if len(path) > 0:
-                print(str(i) + "\t" + path[0].get("text").get("content"))
-                i+=1
+        return self.api_post(self, "databases/" + self.DATABASE_ID + "/query", "")
+        
 
     def api_post(self, api_url_post, data):
         return requests.post(self.api_url_pre + api_url_post, headers=self.payload, data=data).json() 
